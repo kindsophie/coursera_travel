2be828afdb15f0e14c33620a3b0a139247feaecd
kindsophie <ingridsophiekind@googlemail.com>
2024-09-09 15:10:14 -0400

    final

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -20,37 +20,51 @@
         .then(data => {
             let found = false;
 
-            // Suche in Ländern
-            data.countries.forEach(country => {
-                if (country.name.toLowerCase() === input) {
-                    found = true;
-                    displayCountry(country, resultDiv);
-                }
-
-                // Suche in Städten innerhalb des Landes
-                country.cities.forEach(city => {
-                    if (city.name.toLowerCase().includes(input)) {
+            // Spezielle Suchbegriffe überprüfen
+            if (input === 'beach' || input === 'beaches') {
+                found = true;
+                displayBeaches(data.beaches, resultDiv);
+            } else if (input === 'temple' || input === 'temples') {
+                found = true;
+                displayTemples(data.temples, resultDiv);
+            } else if (input === 'city' || input === 'cities') {
+                found = true;
+                displayCities(data.countries, resultDiv);
+            } else if (input === 'country' || input === 'countries') {
+                found = true;
+                displayCountries(data.countries, resultDiv);
+            } else {
+                // Standard-Suche nach Ländern und Städten
+                data.countries.forEach(country => {
+                    // Wenn die Eingabe mit dem Ländernamen übereinstimmt, zeige nur die Städte des Landes an
+                    if (country.name.toLowerCase() === input) {
                         found = true;
-                        displayCity(city, resultDiv);
+                        displayCountry(country, resultDiv);
+                    } else {
+                        // Suche nach Städten im Land
+                        country.cities.forEach(city => {
+                            if (city.name.toLowerCase().includes(input)) {
+                                found = true;
+                                displayCity(city, resultDiv);
+                            }
+                        });
                     }
                 });
-            });
 
-            // Suche in Tempeln
-            data.temples.forEach(temple => {
-                if (temple.name.toLowerCase().includes(input)) {
-                    found = true;
-                    displayItem(temple, resultDiv);
-                }
-            });
+                data.temples.forEach(temple => {
+                    if (temple.name.toLowerCase().includes(input)) {
+                        found = true;
+                        displayItem(temple, resultDiv);
+                    }
+                });
 
-            // Suche in Stränden
-            data.beaches.forEach(beach => {
-                if (beach.name.toLowerCase().includes(input)) {
-                    found = true;
-                    displayItem(beach, resultDiv);
-                }
-            });
+                data.beaches.forEach(beach => {
+                    if (beach.name.toLowerCase().includes(input)) {
+                        found = true;
+                        displayItem(beach, resultDiv);
+                    }
+                });
+            }
 
             if (!found) {
                 resultDiv.innerHTML = 'No recommendation found.';
@@ -62,17 +76,15 @@
         });
 }
 
+// Funktion zur Anzeige eines Landes und seiner Städte (wenn ein Land explizit gesucht wird)
 function displayCountry(country, resultDiv) {
     resultDiv.innerHTML += `<h3>${country.name}</h3>`;
     country.cities.forEach(city => {
-        resultDiv.innerHTML += `
-            <h4>${city.name}</h4>
-            <img src="${city.imageUrl}" alt="${city.name}">
-            <p><strong>Description:</strong> ${city.description}</p>
-        `;
+        displayCity(city, resultDiv);
     });
 }
 
+// Funktion zur Anzeige einer Stadt
 function displayCity(city, resultDiv) {
     resultDiv.innerHTML += `
         <h4>${city.name}</h4>
@@ -81,6 +93,41 @@ function displayCity(city, resultDiv) {
     `;
 }
 
+// Funktion zur Anzeige von Tempeln
+function displayTemples(temples, resultDiv) {
+    resultDiv.innerHTML += `<h3>Temples</h3>`;
+    temples.forEach(temple => {
+        displayItem(temple, resultDiv);
+    });
+}
+
+// Funktion zur Anzeige von Stränden
+function displayBeaches(beaches, resultDiv) {
+    resultDiv.innerHTML += `<h3>Beaches</h3>`;
+    beaches.forEach(beach => {
+        displayItem(beach, resultDiv);
+    });
+}
+
+// Funktion zur Anzeige aller Städte in allen Ländern
+function displayCities(countries, resultDiv) {
+    resultDiv.innerHTML += `<h3>Cities</h3>`;
+    countries.forEach(country => {
+        country.cities.forEach(city => {
+            displayCity(city, resultDiv);
+        });
+    });
+}
+
+// Funktion zur Anzeige aller Länder
+function displayCountries(countries, resultDiv) {
+    resultDiv.innerHTML += `<h3>Countries</h3>`;
+    countries.forEach(country => {
+        resultDiv.innerHTML += `<h4>${country.name}</h4>`;
+    });
+}
+
+// Funktion zur Anzeige eines allgemeinen Elements (Tempel, Strand, etc.)
 function displayItem(item, resultDiv) {
     resultDiv.innerHTML += `
         <h4>${item.name}</h4>
