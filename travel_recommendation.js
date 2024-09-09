733081dcfbc61959a09030e289856b8712404919
kindsophie <ingridsophiekind@googlemail.com>
2024-09-09 09:53:00 -0400

    JavaScript test

---
+++ travel_recommendation.js
@@ -0,0 +1,41 @@
+    const report = document.getElementById("report");
+    const btnSearch = document.getElementById('btnSearch');
+    const btnReset = document.getElementById('btnReset');
+    const recommendations = [];
+
+         function resetForm() {
+          document.getElementById("keywordInput").value = "";
+        }
+
+        function searchKeyword() {
+        const input = document.getElementById('keywordInput').value.toLowerCase();
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = '';
+
+        fetch('travel_recommendation_api.json')
+          .then(response => response.json())
+          .then(data => {
+            const recommendation = data.recommendations.find(item => item.name.toLowerCase() === input);
+
+            if (recommendation) {
+              const name = recommendation.name.join(', ');
+              const imageurl = recommendation.imageUrl.join(', ');
+              const description = recommendation.description;
+
+              resultDiv.innerHTML += `<h2>${recommendation.name}</h2>`;
+              resultDiv.innerHTML += `<img src="${recommendation.imageUrl}" alt="hjh">`;
+
+              resultDiv.innerHTML += `<p><strong>Description:</strong> ${description}</p>`;
+    
+            } else {
+              resultDiv.innerHTML = 'No recommendation found.';
+            }
+          })
+          .catch(error => {
+            console.error('Error:', error);
+            resultDiv.innerHTML = 'An error occurred while fetching data.';
+          });
+      }
+    
+      btnSearch.addEventListener('click', searchRecommendation);
+      btnReset.addEventListener('click', resetForm);
